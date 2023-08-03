78dd17d79a19b6ee8b765190ba052ec93f5a321e
Pedro Oliveira <pedroblizz@outlook.com>
2023-08-03 23:26:44 +0000

    feat: add SignuptController missing password field

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError('email'))
+        return badRequest(new MissingParamError(field))
       }
     }
 
